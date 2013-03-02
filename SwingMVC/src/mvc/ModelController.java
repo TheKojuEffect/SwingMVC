3b9d71cef304b501589d79289b633abb243796a2
Kapil Koju <kplx2.0@gmail.com>
2013-03-02 22:04:36 +0545

    Implements ModelControl interface

--- SwingMVC/src/mvc/ModelController.java
+++ SwingMVC/src/mvc/ModelController.java
@@ -2,4 +2,14 @@ package mvc;
 
 public class ModelController extends AbstractController implements ModelControl {
 
+    @Override
+    public void setNumber(int newNumber) {
+	setModelProperty(ELEMENT_NUMBER_PROPERTY, newNumber);
+    }
+
+    @Override
+    public void setName(String newName) {
+	setModelProperty(ELEMENT_NAME_PROPERTY, newName);
+    }
+
 }
