98220dd50182bc6cd465bce0de00f7c91aa4dd39
Kapil Koju <kplx2.0@gmail.com>
2013-03-02 22:04:20 +0545

    Add setter methods

--- SwingMVC/src/mvc/ModelControl.java
+++ SwingMVC/src/mvc/ModelControl.java
@@ -5,4 +5,7 @@ public interface ModelControl {
     public static final String ELEMENT_NUMBER_PROPERTY = "Number";
     public static final String ELEMENT_NAME_PROPERTY = "Name";
 
+    public void setNumber(int newNumber);
+
+    public void setName(String newName);
 }
