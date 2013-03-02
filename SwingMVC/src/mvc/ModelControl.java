e5dc709eb317a77777fcc14d32f36bc5cb02d17a
Kapil Koju <kplx2.0@gmail.com>
2013-03-02 10:42:53 +0545

    Basic Edit

--- SwingMVC/src/mvc/ModelControl.java
+++ SwingMVC/src/mvc/ModelControl.java
@@ -5,7 +5,4 @@ public interface ModelControl {
     public static final String ELEMENT_NUMBER_PROPERTY = "Number";
     public static final String ELEMENT_NAME_PROPERTY = "Name";
 
-    public void setName(String name);
-
-    public void setNumber(int number);
 }
--- SwingMVC/src/mvc/ModelController.java
+++ SwingMVC/src/mvc/ModelController.java
@@ -1,27 +1,6 @@
 package mvc;
 
-import java.lang.reflect.Method;
 
 public class ModelController extends AbstractController implements ModelControl {
-
-    @Override
-    public void setName(String name) {
-	try {
-	    Method method = getModel().getClass().getMethod("setName",
-		    new Class[] { name.getClass() });
-
-	    method.invoke(getModel(), name);
-
-	} catch (Exception e) {
-	    System.err.println("Method invocation error");
-	    e.printStackTrace();
-	}
-    }
-
-    @Override
-    public void setNumber(int number) {
-	// TODO Auto-generated method stub
-
-    }
 
 }
